a22f5a26df77fd4bf5429cbd7c435a5d0b119425
felix <sparkyxu@hotmail.com>
2018-11-20 16:47:30 -0800

    home page

--- header.php
+++ header.php
@@ -34,10 +34,7 @@
 
 
 
-				<nav id="site-navigation" class="main-navigation" role="navigation">
-					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php echo esc_html( 'Primary Menu' ); ?></button>
-					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
-				</nav><!-- #site-navigation -->
+				
 			</header><!-- #masthead -->
 
 			<div id="content" class="site-content">
