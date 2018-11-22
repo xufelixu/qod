c5f5728d18d625fc8708f2c70c37a85175e453a4
felix <sparkyxu@hotmail.com>
2018-11-22 10:03:16 -0800

    post form js

--- js/custom.js
+++ js/custom.js
@@ -1,43 +1,81 @@
-(function ($) {
-    $(document).ready(function () {
-        // console.log('works');
-        //get a random post and append content to the DOM
+(function($) {
+	$(document).ready(function() {
+		let lastPage = '';
 
-        $('#new-quote-button').on('click', function (event) {
-            event.preventDefault();
-            getQuote();
-            // console.log('click');
-        });
+		// console.log('works');
+		//get a random post and append content to the DOM
 
-        function getQuote() {
-            $.ajax({
-                method: 'GET',
-                url: qod_vars.rest_url + 'wp/v2/posts?filter[orderby]=rand&filter[posts_per_page]=1'
-            }).done(function (data) {
+		$('#new-quote-button').on('click', function(event) {
+			event.preventDefault();
+			getQuote();
+			// console.log('click');
+		});
 
-                var post = data.shift();
+		function getQuote() {
+			lastPage = document.URL;
 
-                var newContent = post.content.rendered;
-                var newTitle = post.title.rendered;
-                $('.entry-content').empty();
-                $('.entry-content').append(newContent);
-                $('entry-title').empty();
-                $('entry-title').append('&mdash;' + newTitle);
+			$.ajax({
+				method: 'GET',
+				url: qod_vars.rest_url + 'wp/v2/posts?filter[orderby]=rand&filter[posts_per_page]=1'
+			})
+				.done(function(data) {
+					const quote = data.shift();
 
+					var newContent = quote.content.rendered;
+					var newTitle = quote.title.rendered;
+					$('.entry-content').empty();
+					$('.entry-content').append(newContent);
+					$('.entry-title').empty();
+					$('.entry-title').append('&mdash;' + newTitle);
 
-                //apend content to the DOM e.g. replace the quote content with rest API content
-                // console.log(data);
+					//apend content to the DOM e.g. replace the quote content with rest API content
+					// console.log(data);
 
+					// console.log(data[0]);
 
-            }).fail(function (err) {
+					//get slug
+					history.pushState(null, null, qod_vars.home_url + '/' + quote.slug);
+				})
+				.fail(function(err) {
+					alert('It is fine to celebrate success but it is more important to heed the lessons of failure.');
+					// console.log(err);
+				});
+		} //end of get quote
 
-                alert('It is fine to celebrate success but it is more important to heed the lessons of failure.');
-                // console.log(err);
-            });
+		$(window).on('popstte', function() {
+			window.location.replace(lastPage);
+		}); // outside the function
 
-        }
+		//submit a form and create a new quote post
 
-    });
+		$('quote-submission-form').on('sumbit', function() {
+			event.preventDefault();
 
+			postQuote();
+			// get values of your form inputs
+			//
+			//e.g const quoteTitle=$('#form-id').val();
+		});
 
+		function postQuote() {
+			$.ajax({
+				method: 'post',
+				url: qod_vars.rest_url + 'wp/v2/posts',
+				data: {
+					title: 'quoteTitle'
+				},
+
+				beforeSend: function(xhr) {
+					xhr.setRequestHeader('X-WP-Nonce', red_vars.wpapi_nonce);
+				}
+			})
+				.done(function(response) {
+					//.sideup the form
+					//append a success msg
+				})
+				.fail(function() {
+					//output a msg for the user saying something went wrong
+				});
+		}
+	}); //end of doc ready
 })(jQuery);
